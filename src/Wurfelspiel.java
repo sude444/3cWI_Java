9494e7fc2571d761505e004a26bbdc81e1795553
sude <sude.bayar@student.htldornbirn.at>
2022-09-28 11:38:32 +0200

    Wurfelspiel

--- src/Wurfelspiel.java
+++ src/Wurfelspiel.java
@@ -8,14 +8,44 @@ public class Wurfelspiel
     {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        int dice = 0;
+        int diceTotal1 = 0;
+        int diceTotal2 = 0;
 
-        int wurfel = 0;
-
-        wurfel = scanner.nextInt();
-        for(int i = 0; i < 100; i++)
+        //User
+        System.out.println();
+        System.out.print("You:\n--------------------");
+        for(int i = 0; i < 6; i++)
         {
-            wurfel = random.nextInt(1, 6);
+            scanner.nextLine();
+            dice = random.nextInt(1, 7);
+            System.out.print(dice);
+            diceTotal1 = diceTotal1 + dice;
         }
 
+
+        //Computer
+        scanner.nextLine();
+        System.out.println("\nComputer:\n--------------------");
+        for(int i = 0; i < 6; i++)
+        {
+            dice = random.nextInt(1, 7);
+            System.out.print(dice + ", ");
+            diceTotal2 = diceTotal2 + dice;
+        }
+
+        //result
+        scanner.nextLine();
+        System.out.println("\nUser dice total: \n--------------------\n" + diceTotal1);
+        System.out.println("\nComputer dice total: \n--------------------\n" + diceTotal2);
+
+        if(diceTotal1 < diceTotal2)
+        {
+            System.out.println("\nWinner:\n--------------------\nComputer won!!");
+        }
+        else
+        {
+            System.out.println("\nWinner:\n--------------------\nUser won!!");
+        }
     }
 }
