6636fd873f9833f0299bfcbaad4c93263cefe152
sude <sude.bayar@student.htldornbirn.at>
2022-09-28 12:11:59 +0200

    TicTacToe

---
+++ src/TicTacToe.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class TicTacToe
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to TikTacToe\n--------------------");
+        System.out.println("Here is your Field:");
+        System.out.println("| | | |\n| | | |\n| | | |\n--------------------");
+
+    }
+}
