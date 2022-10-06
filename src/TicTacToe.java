19a26538899ffb9a3d34fbcd1e2925d8d98e5ba7
sude <sude.bayar@student.htldornbirn.at>
2022-10-06 09:31:38 +0200

    TicTacToe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -14,10 +14,8 @@ public class TicTacToe {
 
         char[][] board = new char[3][3];
 
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
             }
         }
@@ -26,21 +24,18 @@ public class TicTacToe {
         boolean isPlayer1 = true;
         char symbol = ' ';
         int count = 0;
+        boolean gameFinished = false;
 
-        while (true)
-        {
+        while (gameFinished == false) {
             if (isPlayer1) {
                 symbol = 'x';
             } else {
                 symbol = 'o';
             }
 
-            if(isPlayer1)
-            {
+            if (isPlayer1) {
                 System.out.println("****************************\nit's " + Player1 + "'s turn. (x)");
-            }
-            else
-            {
+            } else {
                 System.out.println("****************************\nIt's " + Player2 + "'s turn. (o)");
             }
 
@@ -53,94 +48,73 @@ public class TicTacToe {
             System.out.println("Enter a column ( 0, 1, 2 ) ");
             column = scanner.nextInt();
 
-            if (row < 0 || column < 0 || row > 3 || column > 3)
-            {
+            if (row < 0 || column < 0 || row > 3 || column > 3) {
                 System.out.println("That isn't possible!!");
-            } else if (board[row][column] != '-')
-            {
+            } else if (board[row][column] != '-') {
                 System.out.println("!!!This field is already full!!!");
 
-            }
-            else
-            {
+            } else {
 
             }
 
             board[row][column] = symbol;
             drawBoard(board);
 
-            if(hasWon(board) == 'x')
-            {
+            if (hasWon(board) == 'x') {
                 System.out.println(Player1 + " has won!!!");
-            }
-            else if(hasWon(board) == 'o')
-            {
+                gameFinished = true;
+            } else if (hasWon(board) == 'o') {
                 System.out.println(Player2 + " has won!!!");
-            }
-            else
-            {
+                gameFinished = true;
+            } else {
                 //nobody won
             }
 
-            if(isPlayer1)
-            {
+            if (isPlayer1) {
                 isPlayer1 = false;
-            }
-            else
-            {
+            } else {
                 isPlayer1 = true;
             }
 
         }
     }
 
-    public static void drawBoard(char[][] board)
-    {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
+    public static void drawBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j]);
             }
             System.out.println();
         }
     }
 
-    public static char hasWon (char[][] board)
-    {
-       //Row
-      for (int i = 0; i < 3; i++)
-      {
-            if(board[i][0] == board[1][1] && board[i][1] == board[i][2] && board[i][0] != '-')
-            {
-                return board[i][0];
+    public static char hasWon(char[][] board) {
+        //Row
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0] == board[row][1] && board[row][1] == board[row][2] && board[row][0] != '-') {
+                return board[row][0];
             }
-      }
-
-      //col
-      for(int j = 0; j < 3; j++)
-      {
-        if(board[j][0] == board[1][1] && board[j][1] == board[j][2] && board[j][0] != '-')
-        {
-            return board[0][j];
         }
-      }
 
-      //diag
-      if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-')
-      {
-          return board[0][0];
-      }
-      if(board[2][0] == board[1][1] && board[1][1] == board[2][2] && board[2][0] !='-')
-      {
-          return board[2][0];
-      }
+        //col
+        for (int column = 0; column < 3; column++) {
+            if (board[0][column] == board[1][column] && board[1][column] == board[2][column] && board[column][0] != '-') {
+                return board[0][column];
+            }
+        }
 
-      return '-';
+        //diag
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-') {
+            return board[0][0];
+        }
+        if (board[2][0] == board[1][1] && board[1][1] == board[2][2] && board[2][0] != '-') {
+            return board[2][0];
+        }
+
+        return '-';
 
 
     }
-
 
 
 }
