a70dd0ed0b778f6f15a37e581edd3d0bbfd8fee2
sude <sude.bayar@student.htldornbirn.at>
2022-11-07 11:58:01 +0100

    encryption

--- src/WorterRaten.java
+++ src/WorterRaten.java
@@ -6,43 +6,54 @@ public class WorterRaten
 
     public static void main(String[] args)
     {
-        String guesses = random.nextLine();
+        String[] word = { "katze", "java", "programmieren" };
         int attempts = 10;
-        int wordLength;
-        boolean solved;
-        Scanner userInput = new Scanner(System.in);
+        String letter;
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
 
-
-
-        for(int i = 1; i <= 20; i++)
-            System.out.print("\n");
-
-
-        Scanner userLetter = new Scanner(System.in);
-        String letter;
-
         System.out.print("Word to guess: ");
-        for (int i = 0; i < guesses.length(); i++)
+        for (char i = 0; i < word.length; i++)
         {
-            System.out.print("_");
+            if ( )
+            {
+
+            }
         }
 
         while (attempts <= 10 && attempts > 0)
         {
             System.out.println("\nAttempts left: " + attempts);
             System.out.print("Enter letter: ");
+            letter = scanner.nextLine();
 
             attempts--;
         }
 
         System.out.println("\n---------------------------");
-        System.out.println("Sorry you didn't find the mystery word!");
-        System.out.println("It was \"" + guesses + "\"");
-
+        System.out.println("Sorry you didn't find the word!");
+        System.out.println("It was \"" + word + "\"");
     }
 
+    public static String longestString(String[] str)
+    {
+        String longest = "";
+        System.out.print("Word to guess: ");
+        for (int i = 0; i < str.length; i++)
+        {
+            System.out.print("_");
+            if (str[i].length() > longest.length())
+            {
+                longest = str[i];
+
+            }
+        }
+        return longest;
+    }
+
+
 }
+
 
 
