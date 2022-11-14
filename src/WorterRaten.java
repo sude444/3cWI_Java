517d19def43dbdfecd4ecf8897baefa6492315e9
sude <sude.bayar@student.htldornbirn.at>
2022-11-14 12:54:28 +0100

    WörterRaten

--- src/WorterRaten.java
+++ src/WorterRaten.java
@@ -40,23 +40,44 @@ public class WorterRaten {
                             playerGuess[i] = input;
                         }
                     }
-                    if(wordIsGuessed){
+                    if (wordIsGuessed) {
                         wordIsGuessed = true;
                         System.out.println("Congratulations");
                     }
                 }
             }
-            if(!wordIsGuessed){
+            if (!wordIsGuessed) {
                 System.out.println("You ran out of guesses.");
+            }
+            else{
+                System.out.println("You won!!");
             }
 
             System.out.println("Would you like to play again? (yes/no) ");
             String choice = scanner.nextLine();
-            if(choice.equals("no")){
+            if (choice.equals("no")) {
                 playing = false;
             }
         }
     }
+
+    public static void print(char array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isTheWordGuessed(char[] array) {
+        boolean condition = true;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == '_') {
+                condition = false;
+            }
+        }
+        return condition;
+    }
+
 }
 
 
