f687ee707efba8d822a8d78fe4b6d01067984a4f
sude <sude.bayar@student.htldornbirn.at>
2023-02-02 09:45:25 +0100

    OO_Aufgabe14_Rechner
    normaler Rechner programmiert

--- src/at/bsu/OO/Rechner/Rechner.java
+++ src/at/bsu/OO/Rechner/Rechner.java
@@ -14,7 +14,7 @@ public class Rechner{
         boolean stop = false;
 
         do{
-            System.out.println("Was möchten sie tun? \n--------------------\n1: Addieren\n2: subtrahieren\n3: dividieren\n4: multiplizieren");
+            System.out.println("Was möchten sie tun? \n--------------------\n1: Addieren\n2: subtrahieren\n3: Dividieren\n4: Multiplizieren\ns: stoppen");
             selection = scanner.nextInt();
 
             switch (selection){
@@ -23,15 +23,46 @@ public class Rechner{
                     a = scanner.nextDouble();
                     b = scanner.nextDouble();
                     result = a + b;
-                    System.out.println("Die Summe der Zahlen "+ a + " und "+ b+ " ergbit " + result);
+                    System.out.println("Die Summe der Zahlen "+ a + " und "+ b+ " ergbit " + result+"\n--------------------");
                     break;
 
-                case 2: //    
+                case 2: //Subtrahieren
+                    System.out.println("Geben sie zwei Zahlen ein.");
+                    a = scanner.nextDouble();
+                    b = scanner.nextDouble();
+                    result = a - b;
+                    System.out.println("Die Differenz der Zahlen "+ a + " und "+ b+ " ergbit " + result+"\n--------------------");
+                    break;
+
+                case 3: //Division
+                    System.out.println("Geben sie zwei Zahlen ein.");
+                    a = scanner.nextDouble();
+                    b = scanner.nextDouble();
+                    result = a / b;
+                    System.out.println("Der Quotienten der Zahlen "+ a + " und "+ b+ " ergbit " + result+"\n--------------------");
+                    break;
+
+                case 4: //Multiplikation
+                    System.out.println("Geben sie zwei Zahlen ein.");
+                    a = scanner.nextDouble();
+                    b = scanner.nextDouble();
+                    result = a * b;
+                    System.out.println("Das Produkt der Zahlen "+ a + " und "+ b+ " ergbit " + result +"\n--------------------");
+                    break;
+                case 5: //Beenden
+                    stop = true;
+                    break;
+
+                default:
+                System.out.println("Sie muessen eine Zahl von 1 bis 5 eingeben!!!");
+
 
             }
 
         }
-        while(stop = false);
+        while(stop == false);
+
+        scanner.close();
     }
 }
 
