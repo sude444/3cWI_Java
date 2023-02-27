469152e0071edf30725e221f435f35f6a0348cd2
sude <sude.bayar@student.htldornbirn.at>
2023-02-27 11:51:02 +0100

    OO_Aufgabe15_Figuren
    fertig

--- src/at/bsu/OO/Figuren/Main.java
+++ src/at/bsu/OO/Figuren/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        Kreis kreis = new Kreis();
-        Viereck viereck = new Viereck();
-        Quadrat quadrat = new Quadrat();
-        Würfel würfel = new Würfel();
         int selection;
         boolean stop = false;
+        Kreis kreis = new Kreis("k1", 12);
+        Viereck viereck = new Viereck("v1", 13, 20);
+        Quadrat quadrat = new Quadrat("q1", 12);
+        Würfel würfel = new Würfel("w1", 12);
 
         do{
             System.out.println("Welchen Flaecheninhalt moechten Sie?\n1: Kreis\n2: Viereck\n3: Quadrat\n4: Würfel\n5:stoppen\n------------------------------------");
@@ -23,12 +23,18 @@ public class Main {
                     break;
 
                 case 2: //Viereck
+                    System.out.println(viereck.getName());
+                    System.out.println(viereck.getSiteA());
                     break;
 
                 case 3: //Quadrat
+                    System.out.println(quadrat.getName());
+                    System.out.println(quadrat.getSiteA());
                     break;
 
                 case 4: //Würfel
+                    System.out.println(würfel.getName());
+                    System.out.println(würfel.getSiteA());
                     break;
 
                 case 5: //Beenden
