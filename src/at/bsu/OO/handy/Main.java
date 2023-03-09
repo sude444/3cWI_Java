dfa85b1b4bc6278a9fd2a24605b9ef618f876ddc
sude <sude.bayar@student.htldornbirn.at>
2023-03-09 09:30:40 +0100

    OO_Aufgabe16_Handy-Main fast fertig

--- src/at/bsu/OO/handy/Main.java
+++ src/at/bsu/OO/handy/Main.java
@@ -1,4 +1,11 @@
 package at.bsu.OO.handy;
 
 public class Main {
+    SimCard simCard = new SimCard(1, "+43 660 4444444");
+    Camera camera = new Camera(Camera.RESOLUTION.High);
+    MemoryCard memoryCard = new MemoryCard(44);
+    Handy handy = new Handy(simCard, camera, memoryCard);
+
+
+
 }
