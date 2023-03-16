de8e004a7cf9878c46ca87c9edc107af7f2e6d8e
sude <sude.bayar@student.htldornbirn.at>
2023-03-16 09:13:32 +0100

    OO_Aufgabe17_Camera-Main onGoing

--- src/at/bsu/OO/camera/Main.java
+++ src/at/bsu/OO/camera/Main.java
@@ -1,6 +1,10 @@
 package at.bsu.OO.camera;
 
 public class Main {
-    CamManufacturer cm1 = new CamManufacturer("Canon", "Japan");
-    LensManufacturer lm1 = new LensManufacturer("Sony", "Austria");
+    CamManufacturer cm = new CamManufacturer("Canon", "Japan");
+    LensManufacturer lm = new LensManufacturer("Sony", "Austria");
+    Pics p1 = new Pics("Picture1", 02.11, Pics.SIZE.sechsGB);
+    Pics p2 = new Pics("Picture2", 04.04, Pics.SIZE.zweiGB);
+    Pics p3 = new Pics("Picture3", 03.03, Pics.SIZE.vierGB);
+
 }
