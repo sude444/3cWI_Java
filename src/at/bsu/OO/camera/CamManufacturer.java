3515c0a0946f5ab163551cda38550321b7e055fe
sude <sude.bayar@student.htldornbirn.at>
2023-03-16 08:33:28 +0100

    OO_Aufgabe17_Camera-CamManufacturer fertig

--- src/at/bsu/OO/camera/CamManufacturer.java
+++ src/at/bsu/OO/camera/CamManufacturer.java
@@ -1,4 +1,32 @@
 package at.bsu.OO.camera;
 
 public class CamManufacturer {
+
+    private String name;
+    private String country;
+
+
+    //Constructor
+    public CamManufacturer(String name, String country) {
+        this.name = name;
+        this.country = country;
+    }
+
+
+    //Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
