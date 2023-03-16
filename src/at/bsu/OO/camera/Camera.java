93a1f37e1285a35eb01df5ff807e12b68e242e29
sude <sude.bayar@student.htldornbirn.at>
2023-03-16 08:56:45 +0100

    OO_Aufgabe17_Camera-Camera OnGoing

--- src/at/bsu/OO/camera/Camera.java
+++ src/at/bsu/OO/camera/Camera.java
@@ -1,4 +1,39 @@
 package at.bsu.OO.camera;
 
 public class Camera {
+    private String color;
+    private float weight;
+    private int pixel;
+
+    //Constructor
+    public Camera(String color, float weight, int pixel) {
+        this.color = color;
+        this.weight = weight;
+        this.pixel = pixel;
+    }
+
+    //Getter and Setter
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getPixel() {
+        return pixel;
+    }
+
+    public void setPixel(int pixel) {
+        this.pixel = pixel;
+    }
 }
