cb71665c523a88a7d703b7b1c54a2ad58bf4b9d0
sude <sude.bayar@student.htldornbirn.at>
2023-04-13 08:19:05 +0200

    OO_Aufgabe17_Camera-SDcard angefangen
    SDcard angefangen

--- src/at/bsu/OO/camera/SDcard.java
+++ src/at/bsu/OO/camera/SDcard.java
@@ -1,4 +1,15 @@
 package at.bsu.OO.camera;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SDcard {
+    private float capacity;
+    private List<Pics> files;
+    private Pics.SIZE size;
+
+    public SDcard (float capacity){
+        this.capacity = capacity;
+        this.files = new ArrayList<Pics>();
+
+    }
 }
