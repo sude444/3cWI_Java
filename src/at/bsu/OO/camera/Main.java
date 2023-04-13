ef9db101db38a155e9e252e02a6aa582721e2628
sude <sude.bayar@student.htldornbirn.at>
2023-04-13 09:25:18 +0200

    OO_Aufgabe17_Camera-Main ongoing

--- src/at/bsu/OO/camera/Main.java
+++ src/at/bsu/OO/camera/Main.java
@@ -1,10 +1,15 @@
 package at.bsu.OO.camera;
 
 public class Main {
-    CamManufacturer cm = new CamManufacturer("Canon", "Japan");
-    LensManufacturer lm = new LensManufacturer("Sony", "Austria");
-    Pics p1 = new Pics("Picture1", 02.11, Pics.SIZE.sechsGB);
-    Pics p2 = new Pics("Picture2", 04.04, Pics.SIZE.zweiGB);
-    Pics p3 = new Pics("Picture3", 03.03, Pics.SIZE.vierGB);
+
+    public static void main(String[] args) {
+        CamManufacturer cm = new CamManufacturer("Canon", "Japan");
+        LensManufacturer lm = new LensManufacturer("Sony", "Austria");
+        Pics p1 = new Pics("Picture1", 02.11, Pics.SIZE.sechsGB);
+        Pics p2 = new Pics("Picture2", 04.04, Pics.SIZE.zweiGB);
+        Pics p3 = new Pics("Picture3", 03.03, Pics.SIZE.vierGB);
+
+       p1.getInfo();
+    }
 
 }
