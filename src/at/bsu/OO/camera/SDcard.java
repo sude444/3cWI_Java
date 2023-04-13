4c8a5196ba987e7a0cd83e0fcfbc0d7bc3b53c7d
sude <sude.bayar@student.htldornbirn.at>
2023-04-13 09:04:09 +0200

    OO_Aufgabe17_Camera-SDCard fertif
    fertig gemacht

--- src/at/bsu/OO/camera/SDcard.java
+++ src/at/bsu/OO/camera/SDcard.java
@@ -7,9 +7,50 @@ public class SDcard {
     private List<Pics> files;
     private Pics.SIZE size;
 
+
+    //Constuructor
     public SDcard (float capacity){
         this.capacity = capacity;
         this.files = new ArrayList<Pics>();
+    }
 
+
+    public void saveFile(Pics file){
+        if(capacity < file.getSize()){
+            System.out.println("Bild konte nicht gespeichert werden. Speicher ist voll!");
+        }
+        file.getSize();
+        else{
+            this.files.add(file);
+            this.capacity -= file.getSize();
+            System.out.println("File gespeichert");
+        }
+    }
+
+
+    //Setter und Getter
+
+    public float getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(float capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Pics> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<Pics> files) {
+        this.files = files;
+    }
+
+    public Pics.SIZE getSize() {
+        return size;
+    }
+
+    public void setSize(Pics.SIZE size) {
+        this.size = size;
     }
 }
