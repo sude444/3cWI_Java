69b2bf0cf4e2d464a085cefe34149dd0ab774da6
sude <sude.bayar@student.htldornbirn.at>
2023-04-20 09:18:42 +0200

    OO_Aufgabe17_Camera-Main ongoing

--- src/at/bsu/OO/camera/Main.java
+++ src/at/bsu/OO/camera/Main.java
@@ -5,11 +5,15 @@ public class Main {
     public static void main(String[] args) {
         CamManufacturer cm = new CamManufacturer("Canon", "Japan");
         LensManufacturer lm = new LensManufacturer("Sony", "Austria");
-        Pics p1 = new Pics("Picture1", 02.11, Pics.SIZE.sechsGB);
+        SDcard sd = new SDcard(2000);
+        Camera cam = new Camera("red", 10, 1000, cm, lm, sd);
+
+        cam.getInfo();
+        cam.takePic();
+        /*Pics p1 = new Pics("Picture1", 02.11, Pics.SIZE.sechsGB);
         Pics p2 = new Pics("Picture2", 04.04, Pics.SIZE.zweiGB);
         Pics p3 = new Pics("Picture3", 03.03, Pics.SIZE.vierGB);
-
-       p1.getInfo();
+        p1.getInfo();*/
     }
 
 }
